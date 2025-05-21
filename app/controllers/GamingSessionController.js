1979bdd0dc37f4c143f2c013d478be461f59f596
MilenaD <44272975+milusia93@users.noreply.github.com>
2025-05-21 16:26:53 +0200

    zmiana kompa2

--- app/controllers/GamingSessionController.js
+++ app/controllers/GamingSessionController.js
@@ -1,12 +1,58 @@
 const GamingSessionModel = require('../models/GamingSessionModel')
 
 module.exports = {
-    index: (req, res) => {
-        GamingSessionModel.find()
+    index: async (req, res) => {
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 5; 
+        const sortingCategory = req.query.category || 'date';
+        const sortingDirection = req.query.direction || 'descending';
+        const status = req.query.status; 
+        const gameId = req.query.gameId;
+
+        const sortMethods = {
+            none: 1,
+            ascending: 1,
+            descending: -1,
+        };
+
+        let sort = {};
+
+        if (sortingCategory === 'game.name') {
+            sort = { 'game.name': sortMethods[sortingDirection] };
+        } else {
+            sort = { [sortingCategory]: sortMethods[sortingDirection] };
+        }
+
+        const startIndex = (page - 1) * limit;
+
+        const query = {};
+        if (status === "finished") {
+            query.finished = true;
+        } else if (status === "ongoing") {
+            query.finished = false;
+        }
+    
+        if (gameId) {
+            query.game = gameId;
+        }
+
+        const total = await GamingSessionModel.countDocuments();
+
+        GamingSessionModel.find(query)
+            .skip(startIndex)
+            .limit(limit)
+            .sort(sort)
             .populate('game')
             .populate('players')
+            .populate('winner')
             .then((sessions) => {
-                res.status(200).json(sessions);
+                res.status(200).json({
+                    page,
+                    limit,
+                    total,
+                    pages: Math.ceil(total / limit),
+                    data: sessions,
+                });
             })
             .catch((err) => {
                 res.status(500).json({
